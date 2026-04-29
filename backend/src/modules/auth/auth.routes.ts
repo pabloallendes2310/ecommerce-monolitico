fe4f2270d4c9e3fea828ed52784d27cfcc63c21f
pablo allendes <pablo2310.allendes@gmail.com>
2026-04-29 00:02:33 -0400

    fix(route me , con get

--- backend/src/modules/auth/auth.routes.ts
+++ backend/src/modules/auth/auth.routes.ts
@@ -7,6 +7,6 @@ const authController = new AuthController();
 
 router.post("/register", authController.register);
 router.post("/login", authController.login);
-router.post("/me", authMiddleware, authController.me);
+router.get("/me", authMiddleware, authController.me);
 
 export default router;
