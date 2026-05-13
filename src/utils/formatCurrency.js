05e2cc4c070dac9a2645d1ecabfaaab37cbe5f6a
pablo allendes <pablo2310.allendes@gmail.com>
2026-05-13 10:21:39 -0400

    fix(frontend): frontend a carpeta y limpieza de scaffolding

--- src/utils/formatCurrency.js
+++
@@ -1,7 +0,0 @@
-export function formatCurrency(value) {
-  return new Intl.NumberFormat('es-CL', {
-    style: 'currency',
-    currency: 'CLP',
-    maximumFractionDigits: 0,
-  }).format(value)
-}
